871b7a31f8f833a41a3ed4a9bceeaf65f3dc4be2
Jeffery Maju <147191506+JefferyMaju@users.noreply.github.com>
2026-08-07 20:38:02 +0530

    Added a New File

---
+++ KokoEatingBananas.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+        l, r = 1, max(piles)
+        res = r
+        while l <= r:
+            k = (l+r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p/k)
+            if hours <= h:
+                res = min(k,r)
+                r = k-1
+            else:
+                l = k+1
+        return res
